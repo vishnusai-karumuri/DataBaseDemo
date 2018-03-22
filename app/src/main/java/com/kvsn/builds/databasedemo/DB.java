3a4eab218754c282eec9c768f32a9559bfab65ce
KVSN <vishnusai.karumuri@gmail.com>
2018-03-22 16:21:08 +0530

    commit 2

--- app/src/main/java/com/kvsn/builds/databasedemo/DB.java
+++ app/src/main/java/com/kvsn/builds/databasedemo/DB.java
@@ -1,8 +1,10 @@
 package com.kvsn.builds.databasedemo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 /**
  * Created by KVSN on 3/22/2018.
@@ -21,7 +23,17 @@ public class DB extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-
+        String s = "create table info (name text , mob integer)";
+        db.execSQL(s);
+    }
+    public void insertvalues(String s1 , long l)
+    {
+     SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("name" ,s1);
+        cv.put("mob",l);
+        db.insert("info" , null ,cv);
+        Toast.makeText(ct, "Insert Completed", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
